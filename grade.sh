e2f16c0e373e1ad1e58cd03f7bdfd86627860230
AlainZhangStudent <156254247+AlainZhangStudent@users.noreply.github.com>
2024-03-06 14:32:35 -0800

    Create grade.sh

---
+++ grade.sh
@@ -0,0 +1,32 @@
+if [[ -f student-submission/ListExamples.java ]]
+then
+    cp student-submission/ListExamples.java grading/
+    cp TestListExamples.java grading-area/
+else
+    echo "Missing student-submission/ListExamples.java, did you forget the file or misname it?"
+    exit 1 #nonzero exit code to follow convention
+fi
+
+pwd
+cd grading
+pwd
+
+# what's the classpath argument going to be?
+# /home/list-examples-grader/grader/../lib/*.jar refers to the jars
+CPATH='.:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar'
+javac -cp $CPATH *.java
+
+if [[ $? -ne 0 ]]
+then
+  echo "The program failed to compile, see compile error above"
+  exit 1
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > junit-output.txt
+
+lastline=$(cat junit-output.txt | tail -n 2 | head -n 1)
+tests=$(echo $lastline | awk -F'[, ]' '{print $3}')
+failures=$(echo $lastline | awk -F'[, ]' '{print $6}')
+successes=$((tests - failures))
+
+echo "Your score is $successes / $tests"
